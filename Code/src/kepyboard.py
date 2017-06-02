fe51b618c9f1e21f0713978ab0c6e173a8b19c17
anonymous <abdelrahman.oh@gmail.com>
2017-06-02 00:31:55 -0400

    Refactored code and added comments for typing functionality

--- Code/src/kepyboard.py
+++ Code/src/kepyboard.py
@@ -2,28 +2,35 @@ import sys
 from PyQt4 import QtGui
 
 
-class ButtonBlock(QtGui.QWidget):
-    def __init__(self, *args):
-        super(QtGui.QWidget, self).__init__()
-        grid = QtGui.QGridLayout()
+# function that returns print function to be used by on_click_listeners
+def print_char(name):
+    def print_char_function():
+        # printing characters on same line
+        print(name, end=""),
+        sys.stdout.flush()
 
+    return print_char_function  # (ButtonBlock --> extends QWidget)
+
+
+class ButtonBlock(QtGui.QWidget):
+    # constructor
+    def __init__(self):
+        super().__init__()
+
+        # creating a grid of buttons
+        grid = QtGui.QGridLayout()
         for i in range(6):
             for j in range(6):
-                buttonName = chr(ord('a') + (i * 6) + j)
-                button = QtGui.QPushButton(buttonName, self)
-                # add to the layout
-                button.clicked.connect(self.make_calluser(buttonName))
+                button_name = chr(ord('a') + (i * 6) + j)
+                button = QtGui.QPushButton(button_name)
+                # adding buttons to grid and creating a listener (signals in python?)
+                button.clicked.connect(print_char(button_name))
                 grid.addWidget(button, i, j)
+        # attaching grid to self
         self.setLayout(grid)
 
-    def make_calluser(self, name):
-        def calluser():
-            print (name, end=""),
-            sys.stdout.flush()
 
-        return calluser
-
-
+# Running gui
 app = QtGui.QApplication(sys.argv)
 buttonBlock = ButtonBlock()
 buttonBlock.show()
