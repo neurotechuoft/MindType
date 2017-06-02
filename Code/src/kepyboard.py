c53cdf323deebf51fc46a214e58fca4c0772e2ac
anonymous <abdelrahman.oh@gmail.com>
2017-06-01 23:33:57 -0400

    added printing functionality to the keyboard

--- Code/src/kepyboard.py
+++ Code/src/kepyboard.py
@@ -1,19 +1,30 @@
 import sys
 from PyQt4 import QtGui
 
+
+class ButtonBlock(QtGui.QWidget):
+    def __init__(self, *args):
+        super(QtGui.QWidget, self).__init__()
+        grid = QtGui.QGridLayout()
+
+        for i in range(6):
+            for j in range(6):
+                buttonName = chr(ord('a') + (i * 6) + j)
+                button = QtGui.QPushButton(buttonName, self)
+                # add to the layout
+                button.clicked.connect(self.make_calluser(buttonName))
+                grid.addWidget(button, i, j)
+        self.setLayout(grid)
+
+    def make_calluser(self, name):
+        def calluser():
+            print (name, end=""),
+            sys.stdout.flush()
+
+        return calluser
+
+
 app = QtGui.QApplication(sys.argv)
-widget = QtGui.QWidget()
-layout = QtGui.QGridLayout()
-
-buttons = {}
-
-for i in range(6):
-    for j in range(6):
-        # keep a reference to the buttons
-        buttons[i, j] = QtGui.QPushButton(chr(ord('a') + (i*6) + j))
-        # add to the layout
-        layout.addWidget(buttons[i, j], i, j)
-
-widget.setLayout(layout)
-widget.show()
+buttonBlock = ButtonBlock()
+buttonBlock.show()
 sys.exit(app.exec_())
