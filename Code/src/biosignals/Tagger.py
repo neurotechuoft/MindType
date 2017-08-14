545643aa62914d026d1e4e235b177f5da162f084
Musa Talluzi <musa.talluzi@gmail.com>
2017-08-14 16:47:28 -0400

    Fix some bugs in Tagger

--- Code/src/biosignals/Tagger.py
+++ Code/src/biosignals/Tagger.py
@@ -1,18 +1,21 @@
 import csv
 
 class Tagger(Biosignal):
-
     # Have access to Controller.tag: 0 REST /1 LEFT /2 RIGHT /3 BOTH
 
-    samples = [[]]
-    data = [[]] # Array of 9 arrays, each of which represents a specific
-                # channel, 0 time, 1-8 channels, 9 tag
+    def __init__(self, sample_rate, controller):
+        # SUPERCLASS
+        BioSignal.__init__(self)
+        self.samples = [[]]
+        self.data = [[]] # Array of 9 arrays, each of which represents a specific
+                    # channel, 0 time, 1-8 channels, 9 tag
+
     def update(self, sample):
         """
             Sample: EEG data sample, array of 9 values
             Store this sample in samples
         """
-        samples.append(sample)
+        self.samples.append(sample)
 
 
     def process(self):
@@ -20,12 +23,12 @@ class Tagger(Biosignal):
             Takes every sample in samples, store its values in data
             and tag it
         """
-        if len(samples) != 0:
-            for sample in samples:
+        if len(self.samples) != 0:
+            for sample in self.samples:
                 for i in range(9):
-                    data[i].append(sample[i])
-                data[9].append(Controller.tag)
-                samples.remove(sample)
+                    self.data[i].append(sample[i])
+                self.data[9].append(Controller.tag)
+                self.samples.remove(sample)
 
     def saveToCsv(data):
         """
@@ -33,9 +36,9 @@ class Tagger(Biosignal):
             Each nested list in data will be a row in data.csv
         """
 
-        with open('data.csv', 'wb') as f:
-            writer = csv.writer(f)
-            writer.writerows(data)
-        
-        
-        f.close()
+        with open('data.csv', 'wb') as file_to_write:
+            writer = csv.writer(file_to_write)
+            writer.writerows(self.data)
+
+
+        file_to_write.close()
