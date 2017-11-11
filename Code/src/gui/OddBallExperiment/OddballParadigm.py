ea1e091d2ed8774b4da5a7a48f7d2acaa3d7f721
Pratham Desai <prathamdesai13@gmail.com>
2017-11-11 13:39:25 -0500

    Add files via upload

--- Code/src/gui/OddBallExperiment/OddballParadigm.py
+++ Code/src/gui/OddBallExperiment/OddballParadigm.py
@@ -3,45 +3,86 @@ Simple binary oddball paradigm
 
 Pratham Desai
 """
-import sys
 import pygame
-
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-surface_width = 500
-surface_height = 500
-delta = 1000
+from random import randint
 
 
-def game():
+def binary_experiment(delay_time=200):
+
+    RED = (255, 0, 0)
+    WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255)
+
+    surface_width = 500 + 30
+    surface_height = 500 + 30
+
+    num_rows = 5
+    num_cols = 5
+    grid_margin = 5
+    grid_width = 100
+    grid_height = 100
+
+    # binary grid set to white
+    grid = [[0 for _ in range(num_rows)] for _ in range(num_cols)]
+
     pygame.init()
 
     surface = pygame.display.set_mode((surface_width, surface_height))
 
-    current_time = pygame.time.get_ticks()
-
-    interval = current_time + delta
-    show = False
-
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
-        current_time = pygame.time.get_ticks()
+        random_row = randint(0, 4)
+        random_col = randint(0, 4)
 
-        if current_time >= interval:
-            interval = current_time + delta
-            show = not show
+        if random_row == 2 and random_col == 2:
 
-        surface.fill(RED)
+            # blue square
+            grid[random_row][random_col] = 2
 
-        if show:
-            pygame.draw.rect(surface, BLUE, (0, 0, surface_width, surface_height))
+        else:
 
-        pygame.display.update()
+            # red square
+            grid[random_row][random_col] = 1
 
+        # refresh color for update
+        color = None
+
+        # loop through grid and assign colors based on values
+        for row in range(num_rows):
+            for col in range(num_cols):
+
+                if grid[row][col] == 1:
+
+                    color = RED
+
+                elif grid[row][col] == 2:
+
+                    color = BLUE
+
+                elif grid[row][col] == 0:
+
+                    color = WHITE
+
+                pygame.draw.rect(surface, color,
+                                 [
+                                     (grid_margin + grid_width) * col + grid_margin,
+                                     (grid_margin + grid_height) * row + grid_margin,
+                                     grid_width,
+                                     grid_height
+                                 ]
+                                 )
+
+        # wait time to refresh grid
+        pygame.time.delay(delay_time)
+
+        # reset grid
+        grid = [[0 for _ in range(num_rows)] for _ in range(num_cols)]
+
+        # update
+        pygame.display.flip()
 
 if __name__ == '__main__':
-    game()
+
+    binary_experiment()
+
+
