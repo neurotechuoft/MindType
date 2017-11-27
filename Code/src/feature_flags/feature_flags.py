6e2311c5a50f843731ccb5c1f5eee9268f7e3866
Sayan Faraz <sayan.faraz@mail.utoronto.ca>
2017-11-26 20:58:34 -0500

    documentation for feature flags

--- Code/src/feature_flags/feature_flags.py
+++ Code/src/feature_flags/feature_flags.py
@@ -1,4 +1,9 @@
 class FeatureFlags():
+    """
+        Feature toggling to allow for trunk-based development for features
+        still in development. Turn on or off the features you want by setting
+        the values to true or false.
+    """
     DEV_TOOLS = True
     GUI = False
     COMMAND_LINE = False
