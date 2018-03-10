c4d4463b655ed120ff39fa37c86217d7a725cb24
blmussati <bebamoon@gmail.com>
2018-03-10 09:43:27 -0500

    Implemented forward propagation of the BPNN. Still need to fix the biases of each layer

--- Code/src/multiclass_motor_classification/bpnn.py
+++ Code/src/multiclass_motor_classification/bpnn.py
@@ -36,13 +36,30 @@ epochs = 50000
 input_size, hidden_size, output_size = 18, 73, 1
 eta = .1
 
+# Might need to change from None to [None, input_size] if we pass a batch of EEG records
+# Need someone to check what is more appropriate to have
 input_EEG = tf.placeholder(tf.float32, None)
 
 # Initialise the weights
+# Not sure the matrix should be (inputXoutput) or (outputXinput). Run the code to check dimensionality conflicts
 w_hidden1 = tf.Variable(tf.truncated_normal(shape=[input_size, hidden_size]))
 w_hidden2 = tf.Variable(tf.truncated_normal(shape=[hidden_size, hidden_size]))
 w_output = tf.Variable(tf.truncated_normal(shape=[hidden_size, output_size]))
 
-# Still need to stack the bias
+# Not sure if we need a vector of biases or a matrix. I guess a vector but might be wrong
+# Need someone to check what is more appropriate to have
+# Currently initialised to dummy values. they should be vectors/matrices of 1s, for how I interpret the paper
+b_hidden1 = 0;
+b_hidden2 = 0;
+b_output = 0;
+
+
+# Forward propagation
+z_1 = tf.add(tf.matmul(input_EEG, w_hidden1), b_hidden1)
+a_hidden1 = tf.sigmoid(z_1)
+z_2 = tf.add(tf.matmul(a_hidden1, w_hidden2), b_hidden2)
+a_hidden2 = tf.sigmoid(z_2)
+z_3 = tf.add(tf.matmul(a_hidden2, w_output), b_output)
+output = tf.sigmoid(z_3)
 
 
