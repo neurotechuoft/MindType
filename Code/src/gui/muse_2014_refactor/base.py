d3ca715483871cb55d88ca22940ead582507a615
Jeremy <jeremy.khoo@mail.utoronto.ca>
2018-03-10 14:35:42 -0800

    Docstrings for base.py

--- Code/src/gui/muse_2014_refactor/base.py
+++ Code/src/gui/muse_2014_refactor/base.py
@@ -1,11 +1,13 @@
-"""Base class for recording streams of data. Adapted for Neurotech Mindtype"""
-# Original Author: Jakub Kaczmarzyk <jakubk@mit.edu>
+"""Base class for recording streams of data. Adapted for Neurotech Mindtype.
+Original Author: Jakub Kaczmarzyk <jakubk@mit.edu>.
+"""
 from __future__ import division, print_function, absolute_import
 import threading
 
 
 class BaseStream(object):
     """Base class for recording streams of data."""
+
     def __init__(self):
         self._active = False
         self._kill_signal = threading.Event()
@@ -19,13 +21,10 @@ class BaseStream(object):
         self.data.append(row)
 
     def _record_data_indefinitely(self, inlet):
-        """Record data to list, and correct for time differences between
-        machines.
+        """Appends corrected timestamp to data, and records this data to a list.
 
-        Parameters
-        ----------
-        inlet : pylsl.StreamInlet
-            The LabStreamingLayer inlet of data.
+        Args:
+            inlet : a pylsl.StreamInlet or in other words a LabStreamingLayer inlet of data.
         """
         while not self._kill_signal.is_set():
             sample, timestamp = inlet.pull_sample()
@@ -36,16 +35,12 @@ class BaseStream(object):
     def connect(self, target, name):
         """Connect and record data in a separate thread.
 
-        Parameters
-        ----------
-        target : callable
-            The function to execute in the thread.
-        name : str
-            Name for the thread.
+        Args:
+            target: callable; the function to execute in the thread.
+            name: str; name for the thread.
 
-        Raises
-        ------
-        RuntimeError if attempting to connect more than once.
+        Raises:
+            RuntimeError: if attempting to connect more than once.
         """
         if self._active:
             raise RuntimeError("Stream already active.")
@@ -56,25 +51,24 @@ class BaseStream(object):
             self._active = True
 
     def copy_data(self, start_index=None, end_index=None):
-        """Return deep copy `self.data`.
+        """Returns deep copy of `self.data`.
 
-        Parameters
-        ----------
-        start_index : int
-        end_index : int
-        Returns
-        ----------
-        array: list, items from start index to end index. By default, returns all items.
+        Args:
+            start_index: int
+            end_index: int
+
+        Returns:
+            deep_copy: list; data items from start index to end index. By default, returns all items.
         """
         if start_index is None:
-            # Make this a numpy array?
-            tmp = self.data[:]  # Shallow copy.
-            return [row[:] for row in tmp]  # Deep copy.
+            tmp = self.data[:]  # Shallow copy
+            return [row[:] for row in tmp]  # Deep copy
         else:
             current_max = len(self.data)
             if start_index > current_max:
                 print("Start index {} was requested, but list only goes up to {}.".format(start_index, current_max))
             if end_index > current_max:
                 print("end index {} was requested, but list only goes up to {}.".format(end_index, current_max))
-            tmp = self.data[start_index:end_index + 1]  # Shallow copy.
-            return [row[:] for row in tmp]  # Deep copy.
+            tmp = self.data[start_index:end_index + 1]  # Shallow copy
+            deep_copy = [row[:] for row in tmp] # Deep copy
+            return deep_copy
