9028081f7105ca3feeef028850eb9497973e9dfe
Jeremy <jeremy.khoo@mail.utoronto.ca>
2018-03-10 17:23:15 -0800

    Docstring and comments for analysis_rt.py and lda.py

--- Code/src/gui/muse_2014_refactor/lda.py
+++ Code/src/gui/muse_2014_refactor/lda.py
@@ -1,13 +1,21 @@
 """Contains functions for training, loading, and predicting with a LDA classifier."""
-import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import pickle
 
 
 def create_input_target(data):
-    # Given data will be an array with length N containing N tuples of size 2
-    # First entry of each tuple will be the target value, to be appended.
-    # Second entry will be an array of size M (number of channels) x number of features
+    """Format input data for classifier.
+
+    Args:
+        data: array with length N containing N tuples of size 2. First entry of each tuple will be the target value,
+            to be appended. Second entry will be an array of size(M (number of channels) x number of features).
+
+    Returns:
+        inputs: list of length(N*M) which is the number of trials multiplied by number of channels; each item in the
+            list is an ndarray of length(number of features).
+        targets: list of legnth(N*M); each item is the marker (0 or 1). This is repeated in sequences of 4 as the
+            set of 4 channels at the same event corresponds to the same marker.
+    """
     N = len(data)
     targets = []
     inputs = []
@@ -20,23 +28,41 @@ def create_input_target(data):
 
 
 def lda_train(inputs, targets):
-    # uses sklearn to fit a model given inputs and targets
+    """Uses sklearn to fit a model given inputs and targets
+
+    Args:
+        inputs: list containing (N trials * M channels) data segments of length(number of features).
+        targets: list containing (N trials * M channels) of marker data (0 or 1).
+
+    Returns:
+        classifier: LDA classifier object
+    """
     classifier = LinearDiscriminantAnalysis(solver='svd')
     classifier.fit(inputs, targets)
     return classifier
 
 
-def predict(x, classifier):
-    # x is an array with 4 features: x = [e1, e2, e3, e4]
-    return classifier.predict([x])
+def predict(inputs, classifier):
+    """Prediction for every input event.
+
+    Args:
+        inputs: inputs containing (N trials * M channels) data segments of length(number of features).
+        classifier: LDA classifier object.
+
+    Returns:
+        classifier.predict([inputs]): vector of length (N trials * M channels) containing event predictions.
+    """
+    return classifier.predict([inputs])
 
 
 def save(filepath, classifier):
+    """Saves classifier as a pkl file to the provided filepath."""
     with open(filepath, 'wb') as f:
         pickle.dump(classifier, f)
 
 
 def load(filepath):
+    """Returns a classifier based on the provided pkl filepath."""
     with open(filepath, 'rb') as f:
         classifier = pickle.load(f)
         return classifier
