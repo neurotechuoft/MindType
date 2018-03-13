0c1000857606e78e3568e7a6b922567bd06de20f
Jeremy <jeremy.khoo@mail.utoronto.ca>
2018-03-13 13:56:56 -0700

    MarkerStream allows dynamic trial_set length; queues trial_set end index

--- Code/src/gui/muse_2014_refactor/stream_rt.py
+++ Code/src/gui/muse_2014_refactor/stream_rt.py
@@ -284,17 +284,13 @@ class MarkerStream(base.BaseStream):
     """Class for marker stream object, with same structure as MuseEEGStream.
 
     Receives markers over lsl and places them in a queue for analysis.
-
-    Attributes:
-        trial_num: int; number of stimulus trials (flashes) before the object should queue a trigger (containing an end
-            index) for analysis.
     """
 
-    def __init__(self, trial_num=12):
+    def __init__(self):
         super(MarkerStream, self).__init__()
         self.analyze = Queue.Queue()
-        self.trial_num = trial_num
-
+        self.trial_num = 0
+        self.count = 0
         self.connect(self._connect, 'Marker-data')
 
     def _connect(self):
@@ -314,16 +310,43 @@ class MarkerStream(base.BaseStream):
     def _record_data_indefinitely(self, inlet):
         """Record data to list and correct for time differences. Updates marker count to trigger analysis.
 
+        The identifier is the letter/row/column/block/etc... that should be sent as an integer in the first channel.
+        The timestamp of the marker should be sent in the second channel.
+        Self.trial_num sets how many trials should be taken before analysis. The trial_num should be sent with marker
+        data in channel 3 for the FIRST marker. A trial_num of zero means there is no change to the trial_num.
+        Example: Keyboard has n trials. The first trial should send n, while the subsequent trials should send 0s. This
+        tells the program that every n trials, it should queue n events for analysis. If the next set of trials contains
+        m trials, the first marker of the new set should send m, with the subsequent trials sending 0s, etc...
+
         Args:
             inlet: pylsl.StreamInlet; the LabStreamingLayer inlet of data.
         """
+
         while not self._kill_signal.is_set():
+            # Get marker data from inlet
             sample, timestamp = inlet.pull_sample()
+
+            # Get trial num
+            tmp = sample[2]
+
+            # If trial_num is zero, simply increment count
+            if tmp == 0:
+                self.count += 1
+            # If trial_num has changed, update object's trial_num; should only be updated at the FIRST trial of a set
+            else:
+                self.trial_num = tmp
+                self.count = 1
+
+            # update marker data
             time_correction = inlet.time_correction()
             sample.append(timestamp + time_correction)
-            # TODO: update self.trial_num, either through a sample identifier, or externally update self.trial_num
             self._update(sample)
-            # if all rows/columns have been run through once
-            if len(self.data) % self.trial_num == 0:
-                self.add_analysis([self.trial_num, timestamp])
+
+            # If the set of trials finishes, queue last trial_num events for analysis (as a set of epochs)
+            if self.count % self.trial_num == 0:
+                # Get marker index of the last trial in that set
+                marker_end = len(self.data)
+
+                self.add_analysis([self.trial_num, timestamp, marker_end])
                 print('queue updated')
+
