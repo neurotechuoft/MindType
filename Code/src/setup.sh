397b72871273ffb730ffec685717e0c7782eaf46
Karl <karlcui174@gmail.com>
2018-03-18 18:55:03 -0400

    add python venv creation and activation

--- Code/src/setup.sh
+++ Code/src/setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo apt-get install python3-dev python3-venv
+python3 -m venv MindType
 pip3 install -r requirements.txt
 sudo bash -c 'echo KERNEL==\"ttyUSB[0-9]*\",NAME=\"tts/USB%n\",SYMLINK+=\"%k\",GROUP=\"uucp\",MODE=\"0666\" >> /etc/udev/rules.d/50-ttyusb.rules'
 cd openbci_board
