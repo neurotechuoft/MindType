a1245fe762cefd1b0e144624fe7e1c54a8c39438
Musa Talluzi <musa.talluzi@gmail.com>
2018-10-07 17:26:52 -0400

    Refactor creating buttons for letters

--- Code/src/src/components/LetterComponent.js
+++ Code/src/src/components/LetterComponent.js
@@ -5,45 +5,29 @@ class Letters extends Component {
   constructor(props) {
     super(props);
     this.state = { type: props.statement };
+    this.createButtons = this.createButtons.bind(this);
+  }
+
+  createButtons() {
+    let buttons = [];
+    for (let i = 0; i < 26; i++) {
+      var rowNum = i / 6 + 1;
+      var colNum = i % 6 + 1;
+      var letter = (i + 10).toString(36);
+      var className = "entry row" + rowNum + " col" + colNum;
+      buttons.push(<button className={className}>{letter}</button>);
+      if (i !== 0 && (i+1) % 6 === 0) {
+        buttons.push(<br />)
+      }
+    }
+    return buttons;
   }
 
 
   render() {
     return (
       <div className="userInput">
-        <button className="entry row1 col1">a</button>
-        <button className="entry row1 col2">b</button>
-        <button className="entry row1 col3">c</button>
-        <button className="entry row1 col4">d</button>
-        <button className="entry row1 col5">e</button>
-        <button className="entry row1 col6">f</button>
-        <br />
-        <button className="entry row2 col1">g</button>
-        <button className="entry row2 col2">h</button>
-        <button className="entry row2 col3">i</button>
-        <button className="entry row2 col4">j</button>
-        <button className="entry row2 col5">k</button>
-        <button className="entry row2 col6">l</button>
-        <br />
-        <button className="entry row3 col1">m</button>
-        <button className="entry row3 col2">n</button>
-        <button className="entry row3 col3">o</button>
-        <button className="entry row3 col4">p</button>
-        <button className="entry row3 col5">q</button>
-        <button className="entry row3 col6">r</button>
-        <br />
-        <button className="entry row4 col1">s</button>
-        <button className="entry row4 col2">t</button>
-        <button className="entry row4 col3">u</button>
-        <button className="entry row4 col4">v</button>
-        <button className="entry row4 col5">w</button>
-        <button className="entry row4 col6">x</button>
-        <br />
-        <button className="entry-extra-wide row5 col1">____</button>
-        <button className="entry row5 col3">y</button>
-        <button className="entry row5 col4">z</button>
-        <button className="entry-extra-wide row5 col6">&uarr;</button>
-
+        {this.createButtons()}
       </div>
     )
   }
