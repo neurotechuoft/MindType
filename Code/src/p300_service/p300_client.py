eece82341a8841047cb34d35074e4d19bf6c5a68
Karl Cui <karlcui174@gmail.com>
2019-03-20 15:39:57 -0400

    Move streams to client side

--- Code/src/p300_service/p300_client.py
+++ Code/src/p300_service/p300_client.py
@@ -1,57 +1,94 @@
 from socketIO_client import SocketIO
-
-
-def on_connect():
-    print("Connected")
-
-
-def on_disconnect():
-    print("Disconnected")
-
-
-def on_stream_created(*args):
-    def on_type_stream_created(stream_type=args[1], sid=args[0]):
-        print('{} stream created, SID: {}'.format(stream_type, sid))
-    return on_type_stream_created()
-
-
-def on_stream_started(*args):
-    def on_type_stream_started(stream_type=args[1], sid=args[0]):
-        print('{} stream started, SID: {}'.format(stream_type, sid))
-    return on_type_stream_started()
-
-
-def on_retrieve_prediction_results(*args):
-    def on_retrieve_results(results=args[1], sid=args[0]):
-        print(f'Results: {results}')
-    return on_retrieve_results()
-
+from sanic import Sanic
+from eeg_stream import EEGStream
+from marker_stream import MarkerStream
+from ml_stream import MLStream
 
 class P300Client(object):
+
     def __init__(self):
-        results = []
+        self.results = []
+        self.socket_client = None
+        self.streams = {}
+
+    def connect(self, ip, port):
+        self.socket_client = SocketIO(ip, port)
+        self.socket_client.connect()
+
+    def disconnect(self):
+        self.socket_client.disconnect()
+
+    def on_retrieve_prediction_results(self, *args):
+        def on_retrieve_results(sid=args[0], results=args[1]):
+            uid, ts, p300 = results
+            print(f'p300: {p300}')
+        return on_retrieve_results()
+
+    def create_streams(self):
+        self.streams['eeg'] = self._create_eeg_stream()
+        self.streams['marker'] = self._create_marker_stream()
+
+        # TODO: data is only variable between training (ie. first time the app
+        # is opened) and predictions. The contents of data need to be synced
+        # with the front end and database, based on what the user wants.
+        data = {'classifier_path': 'tests/data/classifier.pkl',
+                'test_path': 'tests/data/test_data.pickle',
+                'event_time': 0.4,      # or 0.2?
+                'train': False,
+                'train_epochs': 120,    # 120 for 2 min, 240 for 4 min
+                'get_test': False}
+
+        self.streams['ml'] = self._create_ml_stream(data)
+
+    def start_streams(self):
+        for stream in ['eeg', 'marker', 'ml']:
+            self._start_stream(stream)
+
+    def predict(self, user_id, timestamp, eeg_data):
+        data = (user_id, timestamp, eeg_data)
+        self.socket_client.emit("retrieve_prediction_results", data, self.on_retrieve_prediction_results)
+        self.socket_client.wait_for_callbacks(seconds=1)
+
+    #
+    # Private methods for creating and starting streams
+    #
+
+    def _create_eeg_stream(self):
+        return EEGStream(thread_name='EEG_data', event_channel_name='P300')
+
+    def _create_marker_stream(self):
+        return MarkerStream()
+
+    def _create_ml_stream(self, data):
+        if self.streams.get('eeg') is None:
+            raise Exception(f"EEG stream does not exist")
+        if self.streams.get('marker') is None:
+            raise Exception(f"Marker stream does not exist")
+
+        return MLStream(m_stream=self.streams['marker'],
+                        eeg_stream=self.streams['eeg'],
+                        classifier_path=data['classifier_path'],
+                        test_path=data['test_path'],
+                        analysis_time=data['analysis_time'],
+                        event_time=data['event_time'],
+                        train=data['train'],
+                        train_epochs=data['train_epochs'],
+                        get_test=data['get_test'])
+
+    def _start_stream(self, stream):
+        if self.streams.get(stream) is None:
+            raise RuntimeError("Cannot start {0} stream, stream does not exist".format(stream))
+        else:
+            self.streams[stream].lsl_connect()
 
 
 if __name__ == '__main__':
-    socket_client = SocketIO("localhost", 8001)
-    socket_client.on("eeg_stream_created", on_stream_created)
-    socket_client.on("marker_stream_created", on_stream_created)
-    socket_client.on("ml_stream_created", on_stream_created)
-    socket_client.on("eeg_stream_started", on_stream_created)
-    socket_client.on("marker_stream_started", on_stream_created)
-    socket_client.on("ml_stream_started", on_stream_created)
-    socket_client.on("retrieve_prediction_results", on_retrieve_prediction_results)
-    socket_client.connect()
-    socket_client.emit("create_eeg_stream")
-    socket_client.emit("create_marker_stream")
-    socket_client.emit("create_ml_stream",
-                       {'classifier_path': 'classifer.pkl',
-                        'test_path': 'test_set.pkl',
-                        'analysis_time': 1,
-                        'event_time': 0.2,
-                        'train': True,
-                        'train_epochs': 120,
-                        'get_test': True,
-                        })
-    socket_client.emit("start_marker_stream")
-    socket_client.disconnect()
+    p300_client = P300Client()
+    p300_client.connect("localhost", 8001)
+
+    user_id = 1123
+    timestamp = 1238794
+    eeg_data = ['data']
+    p300_client.predict(user_id, timestamp, eeg_data)
+
+    p300_client.disconnect()
--- Code/src/p300_service/p300_server.py
+++ Code/src/p300_service/p300_server.py
@@ -13,63 +13,6 @@ class P300Service:
         self.sio = socketio.AsyncServer(async_mode='sanic')
         self.app = Sanic()
         self.sio.attach(self.app)
-        self.eeg_streams = {}
-        self.marker_streams = {}
-        self.ml_streams = {}
-
-    async def create_eeg_stream_handler(self, sid):
-        self.eeg_streams[sid] = EEGStream(thread_name='EEG_data', event_channel_name='P300')
-        print("New eeg stream created.")
-        print(self.eeg_streams)
-        await self.sio.emit("eeg_stream_created", sid)
-
-    async def create_marker_stream_handler(self, sid):
-        self.marker_streams[sid] = MarkerStream()
-        print("New marker stream created.")
-        print(self.marker_streams)
-        await self.sio.emit("marker_stream_created", sid)
-
-    async def create_ml_stream_handler(self, sid, data):
-        if sid not in self.eeg_streams:
-            raise Exception(f"eeg stream in channel {sid} does not exist")
-        if sid not in self.marker_streams:
-            raise Exception(f"marker stream in channel {sid} does not exist")
-        self.ml_streams[sid] = MLStream(m_stream=self.marker_streams[sid],
-                                        eeg_stream=self.eeg_streams[sid],
-                                        classifier_path=data['classifier_path'],
-                                        test_path=data['test_path'],
-                                        analysis_time=data['analysis_time'],
-                                        event_time=data['event_time'],
-                                        train=data['train'],
-                                        train_epochs=data['train_epochs'],
-                                        get_test=data['get_test'])
-        print("New ml_stream created.")
-        print(self.ml_streams)
-        await self.sio.emit("ml_stream_created", sid)
-
-    async def eeg_stream_start_handler(self, sid):
-        if sid in self.eeg_streams:
-            eeg_stream = self.eeg_streams[sid]
-        else:
-            raise (RuntimeError, "Cannot start EEG stream with sid {}".format(sid))
-        eeg_stream.lsl_connect()
-        await self.sio.emit("eeg_stream_started", sid, 'eeg')
-
-    async def marker_stream_start_handler(self, sid):
-        if sid in self.marker_streams:
-            marker_stream = self.marker_streams[sid]
-        else:
-            raise Exception("Cannot start marker stream with sid {}".format(sid))
-        marker_stream.lsl_connect()
-        await self.sio.emit("marker_stream_started", sid, 'marker')
-
-    async def ml_stream_start_handler(self, sid):
-        if sid in self.ml_streams:
-            ml_stream = self.ml_streams[sid]
-        else:
-            raise Exception("Cannot start ML stream with sid {}".format(sid))
-        ml_stream.lsl_connect()
-        await self.sio.emit("ml_stream_started", sid, 'ml')
 
     async def retrieve_prediction_results(self, sid):
         if sid in self.ml_streams:
@@ -88,13 +31,6 @@ class P300Service:
 
 
     def initialize_handlers(self):
-        self.sio.on("create_eeg_stream", self.create_eeg_stream_handler)
-        self.sio.on("create_marker_stream", self.create_marker_stream_handler)
-        self.sio.on("create_ml_stream", self.create_ml_stream_handler)
-        self.sio.on("start_eeg_stream", self.eeg_stream_start_handler)
-        self.sio.on("start_marker_stream", self.marker_stream_start_handler)
-        self.sio.on("start_ml_stream", self.ml_stream_start_handler)
-
         self.sio.on("retrieve_prediction_results", self.retrieve_prediction_results_test)
 
 
