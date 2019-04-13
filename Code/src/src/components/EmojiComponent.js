2a1434d0d8941c55c1a06e7e9137832a8cae279e
YanLing Chen <yanlingycchen@gmail.com>
2019-04-12 21:19:11 -0400

    changed entry widths and added row/col labels to new emojis and numbers

--- Code/src/src/components/EmojiComponent.js
+++ Code/src/src/components/EmojiComponent.js
@@ -5,36 +5,44 @@ class Emojis extends Component {
     return (
       <div className="userInput">
         <div>
-         <button className="entry"><span role="img" aria-label="Emoji">😄</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😌</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😀</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😍</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😎</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😡</span></button>
+          <button className="entry row1 col1"><span role="img" aria-label="Emoji">😊</span></button>
+          <button className="entry row1 col2"><span role="img" aria-label="Emoji">😂</span></button>
+          <button className="entry row1 col3"><span role="img" aria-label="Emoji">😉</span></button>
+          <button className="entry row1 col4"><span role="img" aria-label="Emoji">😍</span></button>
+          <button className="entry row1 col5"><span role="img" aria-label="Emoji">😘</span></button>
+          <button className="entry row1 col6"><span role="img" aria-label="Emoji">😋</span></button>
         </div>
         <div>
-         <button className="entry"><span role="img" aria-label="Emoji">😅</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😴</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🙄</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😲</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😋</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😭</span></button>
+          <button className="entry row2 col1"><span role="img" aria-label="Emoji">😠</span></button>
+          <button className="entry row2 col2"><span role="img" aria-label="Emoji">😒</span></button>
+          <button className="entry row2 col3"><span role="img" aria-label="Emoji">😮</span></button>
+          <button className="entry row2 col4"><span role="img" aria-label="Emoji">😎</span></button>
+          <button className="entry row2 col5"><span role="img" aria-label="Emoji">😢</span></button>
+          <button className="entry row2 col6"><span role="img" aria-label="Emoji">😭</span></button>
         </div>
         <div>
-         <button className="entry"><span role="img" aria-label="Emoji">😈</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">😇</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🤑</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🤵</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">👰</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🤳</span></button>
+          <button className="entry row3 col1"><span role="img" aria-label="Emoji">🙈</span></button>
+          <button className="entry row3 col2"><span role="img" aria-label="Emoji">🙉</span></button>
+          <button className="entry row3 col3"><span role="img" aria-label="Emoji">🙊</span></button>
+          <button className="entry row3 col4"><span role="img" aria-label="Emoji">❤️</span></button>
+          <button className="entry row3 col5"><span role="img" aria-label="Emoji">💔</span></button>
+          <button className="entry row3 col6"><span role="img" aria-label="Emoji">🎉</span></button>
         </div>
         <div>
-         <button className="entry"><span role="img" aria-label="Emoji">🙈</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🙉</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🙊</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">🎅</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">❤️️</span></button>
-         <button className="entry"><span role="img" aria-label="Emoji">💔</span></button>
+          <button className="entry row4 col1"><span role="img" aria-label="Emoji">👋</span></button>
+          <button className="entry row4 col2"><span role="img" aria-label="Emoji">✌️</span></button>
+          <button className="entry row4 col3"><span role="img" aria-label="Emoji">👍</span></button>
+          <button className="entry row4 col4"><span role="img" aria-label="Emoji">👎</span></button>
+          <button className="entry row4 col5"><span role="img" aria-label="Emoji">👏</span></button>
+          <button className="entry row4 col6"><span role="img" aria-label="Emoji">🙏</span></button>
+        </div>
+        <div>
+          <button className="entry row5 col1"><span role="img" aria-label="Emoji">⭐</span></button>
+          <button className="entry row5 col2"><span role="img" aria-label="Emoji">✨</span></button>
+          <button className="entry row5 col3"><span role="img" aria-label="Emoji">🔥</span></button>
+          <button className="entry row5 col4"><span role="img" aria-label="Emoji">💯</span></button>
+          <button className="entry row5 col5"><span role="img" aria-label="Emoji">💩</span></button>
+          <button className="entry row5 col6"><span role="img" aria-label="Emoji">🌈</span></button>
         </div>
       </div>
     )
--- Code/src/src/components/NumberComponent.js
+++ Code/src/src/components/NumberComponent.js
@@ -4,18 +4,46 @@ class Numbers extends Component {
   render() {
     return (
       <div className="userInput">
-        <button className="entry-wide">0</button>
-        <button className="entry-wide">1</button>
-        <button className="entry-wide">2</button>
-        <button className="entry-wide">3</button>
-        <button className="entry-wide">4</button>
-        <br />
-        <button className="entry-wide">5</button>
-        <button className="entry-wide">6</button>
-        <button className="entry-wide">7</button>
-        <button className="entry-wide">8</button>
-        <button className="entry-wide">9</button>
-
+        <div>
+          <button className="entry row1 col1">1</button>
+          <button className="entry row1 col2">2</button>
+          <button className="entry row1 col3">!</button>
+          <button className="entry row1 col4">@</button>
+          <button className="entry row1 col5">#</button>
+          <button className="entry row1 col6">$</button>
+        </div>
+        <div>
+          <button className="entry row2 col1">3</button>
+          <button className="entry row2 col2">4</button>
+          <button className="entry row2 col3">%</button>
+          <button className="entry row2 col4">^</button>
+          <button className="entry row2 col5">&amp;</button>
+          <button className="entry row2 col6">~</button>
+          </div>
+        <div>
+          <button className="entry row3 col1">5</button>
+          <button className="entry row3 col2">6</button>
+          <button className="entry row3 col3">+</button>
+          <button className="entry row3 col4">-</button>
+          <button className="entry row3 col5">=</button>
+          <button className="entry row3 col6">*</button>
+        </div>
+        <div>
+          <button className="entry row4 col1">7</button>
+          <button className="entry row4 col2">8</button>
+          <button className="entry row4 col3">:</button>
+          <button className="entry row4 col4">;</button>
+          <button className="entry row4 col5">'</button>
+          <button className="entry row4 col6">"</button>
+        </div>
+        <div>
+          <button className="entry row5 col1">9</button>
+          <button className="entry row5 col2">0</button>
+          <button className="entry row5 col3">(</button>
+          <button className="entry row5 col4">)</button>
+          <button className="entry row5 col4">/</button>
+          <button className="entry row5 col5">\</button>
+        </div>
       </div>
     )
   }
