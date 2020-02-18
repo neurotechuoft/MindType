bd05c5e5a1967c31fb98e47b0d00c59c576ab27d
Dakota McInnis <dakotamcinnis1@gmail.com>
2020-02-18 04:14:20 -0500

    Made Use Of Grid For Final Row Of Letters Keyboard

--- Code/V2/src/Views/Letters.js
+++ Code/V2/src/Views/Letters.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Grid from "./Components/Grid";
-import {CLASSES} from "./Components/Grid/Constants";
 import delete_btn from './assets/delete.png';
 import shift_btn from './assets/shift.png';
 
@@ -8,31 +7,21 @@ class Letters extends Component {
 
   render() {
 
-    let rowSize = 4;
+    let rowSize = 5;
     let colSize = 6;
+
+    let shift = (<img src={shift_btn} className="shift-img"/>);
+    let del = (<img src={delete_btn} className="delete-img"/>);
 
     let characters = [
         'a', 'a', 'c', 'd', 'e', 'f',
         'f', 'h', 'i', 'j', 'k', 'l',
         'm', 'n', 'o', 'o', 'q', 'r',
-        's', 'v', 'v', 'v', 'w', 'w'
+        's', 'v', 'v', 'v', 'w', 'w',
+        'y', 'z', '_', '_', shift, del
     ];
 
-    return (
-      <div>
-            <Grid rowSize={rowSize} colSize={colSize} contents={characters} />
-            <br />
-            <button className={CLASSES.UNSELECTED + " " + CLASSES.ROW + "4" + " " + CLASSES.COL + "0"}>y</button>
-            <button className={CLASSES.UNSELECTED + " " + CLASSES.ROW + "4" + " " + CLASSES.COL + "1"}>z</button>
-            <button className={CLASSES.UNSELECTED + " " + CLASSES.ROW + "4" + " " + CLASSES.COL + "2" + CLASSES.WIDTH + "0"}>____</button>
-            <button className={CLASSES.UNSELECTED + " " + CLASSES.ROW + "4" + " " + CLASSES.COL + "4"}>
-              <img src={shift_btn} className="shift-img"></img>
-            </button>
-            <button className={CLASSES.UNSELECTED + " " + CLASSES.ROW + "4" + " " + CLASSES.COL + "5"}>
-              <img src={delete_btn} className="delete-img"></img>
-            </button>
-      </div>
-    );
+    return (<Grid rowSize={rowSize} colSize={colSize} contents={characters} />);
   }
 }
 
